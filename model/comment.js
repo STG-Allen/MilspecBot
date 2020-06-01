9dfc444f31b5604bb04c2a9600f7004bce046cfb
Sophie <peanutmacaroon@outlook.com>
2020-06-01 22:55:52 +0300

    Get rid of legacy js code

--- model/comment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const commentSchema = new mongoose.Schema({
-    comment: String,
-    author: String
-})
-
-module.exports = mongoose.model('Comment', commentSchema)
