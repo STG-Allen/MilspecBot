70d8d00d8e132e91521e75e4e8e9ae55a1ca3948
Sophie <peanutmacaroon@outlook.com>
2020-06-03 20:56:48 +0300

    Move all messages into another file

--- src/commands/ticketUtils/close.ts
+++ src/commands/ticketUtils/close.ts
@@ -2,63 +2,63 @@ import Discord from "discord.js";
 import Ticket from '../../model/ticket';
 import User from '../../model/user';
 
+import {
+  permissionError,
+  notInGuild,
+  unspecifiedTicket,
+  informOfClosure,
+  ticketStatusAlready,
+  genericActionError,
+} from '../messages/ticket.messages';
+
 export default async function close(message: Discord.Message, ...args: string[]) {
   const embed = new Discord.MessageEmbed().setTitle('Ticket');
   const authorIsStaff = message.member.roles.cache.some(role => role.name === 'Staff');
 
   const [, ticketId] = args;
-
-  const [,, ...solution] = args ?? [,, 'unknown'];
+  const [,, ...solutionParts] = args ?? [,, 'unknown'];
+  const solution = solutionParts.join(' ');
 
   if (!(message.guild && authorIsStaff)) {
     embed.setColor('RED')
-    if (message.guild) {
-      embed.addField('ERROR', "You do not have permission for this command!")
-    } else {
-      embed.addField('ERROR', 'You must be in a discord guild to run this command!')
-    }
+    embed.addField('ERROR', message.guild ? permissionError : notInGuild);
     return message.channel.send({ embed }).catch(console.error);
   }
 
   if (!ticketId) {
     embed.setColor('RED')
-    embed.addField('ERROR', 'Please specify a ticket number to close!')
-    if (message.guild) {
-      return message.channel.send({ embed }).catch(console.error);
-    } else {
-      return message.author.send({ embed }).catch(console.error);
-    }
+    embed.addField('ERROR', unspecifiedTicket('close'));
+    return message.channel.send({ embed }).catch(console.error);
   }
 
   try {
     const ticket = await Ticket.findOne({ number: ticketId });
 
-    if (['open', 'hold'].includes(ticket.status)) {
-      await Ticket.findOneAndUpdate({ number: ticketId }, {
-        $set: { status: 'closed', solution: solution.join(' ') }
-      });
-      embed.setColor('GREEN');
-      embed.addField('Closed Ticket', ticket.number);
-      if (message.guild) {
-        message.channel.send({ embed });
-      } else {
-        message.author.send({ embed });
-      }
-
-      const user = await User.findOne({ 'ticketNums': { $in: [ticketId] } });
-      const discordUser = message.client.users.cache.get(user.discordId)
-      return discordUser.send([
-        `Your ticket with ID ${ticketId} has been closed.`,
-        'Provided solution:',
-        '```' + solution + '```'
-      ]).catch(console.error);
-    } else {
-      return message.reply(`Ticket ${ticketId} is already closed.`);
+    /**
+     * TODO: Perhaps remove this bit entirely to allow for solution updates.
+     */
+    if (!['open', 'hold'].includes(ticket.status)) {
+      return message.reply(ticketStatusAlready('closed', ticketId));
     }
+
+    await Ticket.findOneAndUpdate({ number: ticketId }, {
+      $set: { status: 'closed', solution }
+    });
+
+    embed.setColor('GREEN');
+    embed.addField('Closed Ticket', ticket.number);
+
+    message.channel.send({ embed }).catch(console.error);
+
+    const user = await User.findOne({ 'ticketNums': { $in: [ticketId] } });
+    const discordUser = message.client.users.cache.get(user.discordId)
+
+    return discordUser.send(informOfClosure(ticketId, solution)).catch(console.error);
+
   } catch(ex) {
     console.trace(ex);
     embed.setColor('RED');
-    embed.addField('Error', `Could not close ticket no. ${ticketId}.`);
+    embed.addField('Error', genericActionError('close', ticketId));
     return message.reply(embed);
   }
 
